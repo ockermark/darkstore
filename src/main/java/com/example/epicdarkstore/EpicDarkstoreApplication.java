6a026c2c52e6f62d0695329f0b04393d4c128b92
ockermark <thomas.ockermark@gmail.com>
2021-11-09 08:37:56 +0100

    Hello world

---
+++ src/main/java/com/example/epicdarkstore/EpicDarkstoreApplication.java
@@ -0,0 +1,15 @@
+package com.example.epicdarkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicDarkstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicDarkstoreApplication.class, args);
+    }
+
+
+
+}
