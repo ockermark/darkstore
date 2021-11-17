139953b768cc86d301a77d001c129f04d4b1cbad
Oskar <ozzyoskar@hotmail.com>
2021-11-18 00:01:37 +0100

    added packages and java class User Product Shop

---
+++ src/main/java/com/example/epicdarkstore/entity/Shop.java
@@ -0,0 +1,20 @@
+package com.example.epicdarkstore.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+
+public class Shop {
+
+
+
+
+
+
+}
