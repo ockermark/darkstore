4255798209d4a66829d16f089533eb5341810959
Oskar <ozzyoskar@hotmail.com>
2021-11-17 23:01:24 +0100

    added packages and java class User Product

---
+++ src/main/java/com/example/epicdarkstore/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.epicdarkstore.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String userType;
+
+
+
+}
