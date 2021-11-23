e14d36ac9edda303b5db9855c4157b85af40c18d
Oskar <ozzyoskar@hotmail.com>
2021-11-23 09:33:01 +0100

    added packages and java class User Product Shop

---
+++ src/main/java/com/example/epicdarkstore/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.epicdarkstore.repository;
+
+import com.example.epicdarkstore.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    List<Product>findByProductName(String productName);
+
+
+
+}
